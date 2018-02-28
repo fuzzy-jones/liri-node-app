2703d1c98e62b3c5d4a69bc4e11c569e4d34cd4c
FuzzyJones <donald.jones83@gmail.com>
2018-02-27 21:48:45 -0500

    re-did liri.js file, almost finished

--- liri.js
+++ liri.js
@@ -21,22 +21,13 @@ var command = process.argv[2];
 
 // need to listen for multiple words in arg
 var nodeArgs = process.argv;
-var songName = " ";
-// Loop through all the words in the node argument at process.arv[3] and beyond for songs that are longer than one word, and chain together with +'s
+var songOrMovieName = "";
+// Loop through all the words in the node argument at process.arv[3] and beyond for songs and movies that are longer than one word, and chain together with +'s
 for (var i = 3; i < nodeArgs.length; i++) {
   if (i > 3 && i < nodeArgs.length) {
-    songName = songName + "+" + nodeArgs[i];
+    songOrMovieName = songOrMovieName + "+" + nodeArgs[i];
   } else {
-    songName += nodeArgs[i];
-  }
-};
-// Loop through all the words in the node argument at process.arv[3] and beyond for movies that are longer than one word, and chain together with +'s
-var movieName = "";
-for (var i = 3; i < nodeArgs.length; i++) {
-  if (i > 3 && i < nodeArgs.length) {
-    movieName = movieName + "+" + nodeArgs[i];
-  } else {
-    movieName += nodeArgs[i];
+    songOrMovieName += nodeArgs[i];
   }
 };
 
@@ -47,94 +38,91 @@ var movieThis = "movie-this";
 var doIt = "do-what-it-says";
 
 // default song if no arg is passed in the command line
-var defaultSong = "hush";
+var defaultSong = "the-sign";
 // default movie is no arg is passed in the command line
-var defaultMovie = "hush";
+var defaultMovie = "mr.+nobody";
 
-
-// using the twitter package to search for the twitter account and access that tweets from the account
-client.get('search/tweets', {q: 'UNCCBCfuzzy'}, function(error, tweets, response) {
-    // store the data statuses in a new var
-    var theTweets = tweets.statuses;
-    // if the command for 3rd arg is 'my-tweets' console loge the tweets, else do nothing
-    if (command === myTweets) {
+function twitterRequest() {
+    // using the twitter package to search for the twitter account and access that tweets from the account
+    client.get('search/tweets', {q: 'UNCCBCfuzzy'}, function(error, tweets, response) {
+        // store the data statuses in a new var
+        var theTweets = tweets.statuses;       
+        // the assignment says to display for 20 recent tweets, but I only have 5 tweets and length of 20 was causing error, so I did it for length
         for (var i = 0; i < theTweets.length; i++) {
         console.log("-"+theTweets[i].text);
-        }  
-    } else {
-        return;
-    }
-});
+        }   
+    });
+};
 
-
-spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   
-    var theSpotify = data.tracks.items[0];
-    if (command === spotifySong && songName !== undefined) {
-        // not sure what the null and 2 mean
+function spotifyRequest() {
+    spotify.search({ type: 'track', query: songOrMovieName, limit: 1 }, function(err, data) {
+        if (err) {
+        return console.log('Error occurred: ' + err);
+        }
+        // targeting the data in the object up until where the date I want located to shorten up the console log
+        var theSpotify = data.tracks.items[0];
+        // not sure what the null and 2 mean, but console logging the necessary data from the json object
         console.log(JSON.stringify("Artist: " + theSpotify.album.artists[0].name, null, 2));
         console.log(JSON.stringify("Song: " + theSpotify.name, null, 2));
-        console.log(JSON.stringify("URL: " + theSpotify.album.external_urls.spotify, null, 2));
-        console.log(JSON.stringify("Album: " + theSpotify.album.name, null, 2));
-    } else if (command === doIt) {
-        // if command is do-what-it-says then read the txt file and display results for song in file
-        fs.readFile("random.txt", "utf8", function(error, data) {
-            // If the code experiences any errors it will log the error to the console.
-            if (error) {
-            return console.log(error);
-            }
-            // makes it a data array then split it
-            var dataArr = data.split(",");
-            // store the data in the array for the song name in var thatWay
-            var thatWay = dataArr[1];
-            songName = thatWay;
-            console.log(songName);
-            // console.log(JSON.stringify("Artist: " + theSpotify.album.artists[0].name, null, 2));
-            // console.log(JSON.stringify("Song: " + theSpotify.name, null, 2));
-            // console.log(JSON.stringify("URL: " + theSpotify.album.external_urls.spotify, null, 2));
-            // console.log(JSON.stringify("Album: " + theSpotify.album.name, null, 2));
-        });
-    } else if (command === spotifySong && songName == null) {
-        console.log("no song for you");
-        // (songName = defaultSong);
-        // console.log(songName);
-        // console.log(JSON.stringify("Artist: " + theSpotify.album.artists[0].name, null, 2));
-        // console.log(JSON.stringify("Song: " + theSpotify.name, null, 2));
-        // console.log(JSON.stringify("URL: " + theSpotify.album.external_urls.spotify, null, 2));
-        // console.log(JSON.stringify("Album: " + theSpotify.album.name, null, 2));
+        console.log(JSON.stringify("URL: " + theSpotify.external_urls.spotify, null, 2));
+        console.log(JSON.stringify("Album: " + theSpotify.album.name, null, 2));   
+    });
+};
+
+function omdbRequest() {
+    // Then run a request to the OMDB API with the movie specified
+    request("http://www.omdbapi.com/?t="+songOrMovieName+"&y=&plot=short&apikey=trilogy", function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            // Parse the body of the site and recover the data desired from the json object
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors); 
+        }  
+    });
+};
+
+function fsRequest() {
+    // if command is do-what-it-says then read the txt file and display results for song in file
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+        return console.log(error);
+        }
+        // makes it a data array then split it
+        var dataArr = data.split(",");
+        // store the data in the array for the song name in var thatWay
+        var thatWay = dataArr[1];
+        songOrMovieName = thatWay;
+        spotifyRequest();
+    });
+}
+
+function runRequests() {
+    // if the command for 3rd arg is 'my-tweets' console loge the tweets, else do nothing
+    if (command === myTweets) {
+        twitterRequest();
+    } else if (command === spotifySong && songOrMovieName !== undefined) { 
+        spotifyRequest();
     } 
-    else {
-        return;
-    }   
-});
-
-
-// Then run a request to the OMDB API with the movie specified
-request("http://www.omdbapi.com/?t="+movieName+"&y=&plot=short&apikey=trilogy", function(error, response, body) {
-    
-    // If the request is successful (i.e. if the response status code is 200)
-    if (!error && response.statusCode === 200 && command === movieThis && movieName !== undefined) {
-
-        // Parse the body of the site and recover the data desired
-        console.log("Title: " + JSON.parse(body).Title);
-        console.log("Year: " + JSON.parse(body).Year);
-        console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
-        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-        console.log("Country: " + JSON.parse(body).Country);
-        console.log("Language: " + JSON.parse(body).Language);
-        console.log("Plot: " + JSON.parse(body).Plot);
-        console.log("Actors: " + JSON.parse(body).Actors);
-    } else if (!error && response.statusCode === 200 && command === movieThis && movieName == null) {
-        console.log("no movie for you");
-        // (movieName = defaultMovie);
-        // console.log(movieName);
-    } 
-    else {
-        return;
+    // else if (command === spotifySong && songOrMovieName == null) {
+    //     console.log("no song for you");
+    // } 
+    else if (command === movieThis && songOrMovieName !== undefined) {
+        omdbRequest();
     }
-});
+    // else if (command === movieThis && songOrMovieName == null) {
+    //     console.log("no movie for you");
+    //     // (songOrMovieName = defaultMovie);
+    //     // console.log(songOrMovieName);
+    // } 
+    else if (command === doIt) {
+        fsRequest();
+    }
+};
 
-
+runRequests();
