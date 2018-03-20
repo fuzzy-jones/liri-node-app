755e8c32d52e5e263bea720407791e18b2599b80
FuzzyJones <donald.jones83@gmail.com>
2018-03-20 19:42:05 -0400

    typo

--- liri.js
+++ liri.js
@@ -133,16 +133,4 @@ function runRequests() {
 // run the function for the requests based on the if statements
 runRequests();
 
-// function addToLog() {
-//     fs.appendFile("log.txt", pick + ": " + title + "\n", function(err) {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             console.log("content added to log.txt");
-//         }
-//     });
 
-// }
-// return {
-//     init: init
-// }
